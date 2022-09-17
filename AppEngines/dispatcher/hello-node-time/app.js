2a1103be44f8a7df96f7831eaa83807edc976766
monojit18 <datta.monojit@gmail.com>
2022-09-17 23:27:48 +0530

    appengine - ic

---
+++ AppEngines/dispatcher/hello-node-time/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/time', (req, res) => {
+  const dt = new Date().toLocaleTimeString("en-IN", {timeZone: "Asia/Kolkata"});
+  const msg = `Hello from Node - ${dt}\n`; 
+  res.status(200).send(msg).end();
+});
+
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+module.exports = app;
